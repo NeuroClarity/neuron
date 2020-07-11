c79caa0539220ecdba7de28a3aae92d8794ec7aa
rahuldesai1 <rahuldesai@berkeley.edu>
2020-07-10 23:59:22 -0700

    Big fixes and exception handling for queue messaging

--- messaging/queue_thread.py
+++ messaging/queue_thread.py
@@ -4,16 +4,15 @@ import json
 import boto3
 from .queue_types import QueueName
 
-S3_RAW_DATA_BUCKET = "nc-reviewier-raw-data"
+S3_RAW_DATA_BUCKET = "nc-reviewer-raw-data"
 S3_RESULTS_BUCKET = "nc-review-results"
 
 class QueueThread(threading.Thread):
     def __init__(self, aws_session, queue):
         super(QueueThread, self).__init__()
-        self.sqs = aws_session.client('sqs', region_name="us-west-1") 
-        #self.queueUrl = self.sqs.get_queue_url(QueueName=queue.value)
-        self.queueUrl = "https://sqs.us-west-1.amazonaws.com/471943556279/nc-eye-tracking-data"
-        self.s3 = aws_session.resource('s3')
+        self.sqs = aws_session.client('sqs')
+        self.queueUrl = self.sqs.get_queue_url(QueueName=queue.value)['QueueUrl']
+        self.s3 = aws_session.client('s3')
         # TODO: This needs to be changed to the appropriate neuron classifier 
         if queue == QueueName.EYE_TRACKING: 
             self.processor = lambda x: x 
@@ -23,20 +22,26 @@ class QueueThread(threading.Thread):
     def run(self):
         print("Started consuming from queue", self.queueUrl)
         while (True):
-            messages = self.sqs.receive_message(QueueUrl=self.queueUrl, MaxNumberOfMessages=10, VisibilityTimeout=60, WaitTimeSeconds=20)['Messages']
-            if len(messages) > 0:
-                print("Retrieved message from queue:", messages)
-                for message in messages:
-                    self.handler(message)
-            else:
-                # sleep for some time if there are no messages on the queue (this will prevent the number of requests from blowing up too much)
-                print("No messages on the queue, going to sleep...")
-                time.sleep(60)
+            try:
+                messages = self.sqs.receive_message(QueueUrl=self.queueUrl, MessageAttributeNames=['Key'], MaxNumberOfMessages=10, VisibilityTimeout=60, WaitTimeSeconds=20)['Messages']
+                if len(messages) > 0:
+                    print("Retrieved messages from queue:", messages)
+                    for message in messages:
+                        self.handler(message)
+                else:
+                    # sleep for some time if there are no messages on the queue (this will prevent the number of requests from blowing up too much)
+                    print("No messages on the queue, going to sleep...")
+                    time.sleep(60)
+            except Exception as e:
+                print("ERROR: Uncaught exception while processing queue: {}".format(e))
 
-    def handler(self, messages):
+
+    def handler(self, message):
         # get data from S3
-        key = message['MessageAttributes']['Key']
+        key = message['MessageAttributes']['Key']['StringValue']
         data = self.get_data_from_key(key)
+        if not data:
+            return
         # process data in neuron
         results = self.processor(data)
         # write results to S3
@@ -44,11 +49,16 @@ class QueueThread(threading.Thread):
         self.sqs.delete_message(self.queueUrl, messages['ReceiptHandle'])
 
     def get_data_from_key(self, key):
-        return self.s3.get_object(bucket=S3_RAW_DATA_BUCKET, key=key)
+        try:
+            return self.s3.get_object(Bucket=S3_RAW_DATA_BUCKET, Key=key)
+        except botocore.errorfactory.NoSuchKey:
+            print("ERROR: Cannot find key {}".format(key))
+        return None
+
 
     def publish_results_to_s3(self, old_key, results):
         key = self._get_results_key_from_data_key(old_key)
-        self.s3.put_object(body=json.dumps(results), bucket=self.bucket, key=key)
+        self.s3.put_object(body=json.dumps(results), Bucket=self.bucket, Key=key)
         return
 
     def _get_results_key_from_data_key(self, data_key):
